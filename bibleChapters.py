4b67a14425cbaa522fecb6d051f59eacd008d7b1
MikeMape <98138866+MikeMape@users.noreply.github.com>
2022-06-05 16:23:50 -0400

    Add files via upload

---
+++ bibleChapters.py
@@ -0,0 +1,209 @@
+chapter = 1
+
+while chapter <= 1189:
+    # This first section is known as the 'Pentateuch'
+    while chapter <= 50:
+        print("Genesis " + str(chapter))
+        chapter = chapter + 1
+    while 50 < chapter <= 90:
+        print("Exodus " + str(chapter - 50))
+        chapter = chapter + 1
+    while 90 < chapter <= 117:
+        print("Leviticus " + str(chapter - 90))
+        chapter = chapter + 1
+    while 117 < chapter <= 153:
+        print("Numbers " + str(chapter - 117))
+        chapter = chapter + 1
+    while 153 < chapter <= 187:
+        print("Deuteronomy " + str(chapter - 153))
+        chapter = chapter + 1
+    # Here are the 'Historical Books'
+    while 187 < chapter <= 211:
+        print("Joshua " + str(chapter - 187))
+        chapter = chapter + 1
+    while 211 < chapter <= 232:
+        print("Judges " + str(chapter - 211))
+        chapter = chapter + 1
+    while 232 < chapter <= 236:
+        print("Ruth " + str(chapter - 232))
+        chapter = chapter + 1
+    while 236 < chapter <= 267:
+        print("1 Samuel " + str(chapter - 236))
+        chapter = chapter + 1
+    while 267 < chapter <= 291:
+        print("2 Samuel " + str(chapter - 267))
+        chapter = chapter + 1
+    while 291 < chapter <= 313:
+        print("1 Kings " + str(chapter - 291))
+        chapter = chapter + 1
+    while 313 < chapter <= 338:
+        print("2 Kings " + str(chapter - 313))
+        chapter = chapter + 1
+    while 338 < chapter <= 367:
+        print("1 Chronicles " + str(chapter - 338))
+        chapter = chapter + 1
+    while 367 < chapter <= 403:
+        print("2 Chronicles " + str(chapter - 367))
+        chapter = chapter + 1
+    while 403 < chapter <= 413:
+        print("Ezra " + str(chapter - 403))
+        chapter = chapter + 1
+    while 413 < chapter <= 426:
+        print("Nehemiah " + str(chapter - 413))
+        chapter = chapter + 1
+    while 426 < chapter <= 436:
+        print("Esther " + str(chapter - 426))
+        chapter = chapter + 1
+    # Here are the 'Books of Wisdom'
+    while 436 < chapter <= 478:
+        print("Job " + str(chapter - 436))
+        chapter = chapter + 1
+    while 478 < chapter <= 628:
+        print("Psalms " + str(chapter - 478))
+        chapter = chapter + 1
+    while 628 < chapter <= 659:
+        print("Proverbs " + str(chapter - 628))
+        chapter = chapter + 1
+    while 659 < chapter <= 671:
+        print("Ecclesiastes " + str(chapter - 659))
+        chapter = chapter + 1
+    while 671 < chapter <= 679:
+        print("Song of Solomon " + str(chapter - 671))
+        chapter = chapter + 1
+    while 679 < chapter <= 745:
+        print("Isaiah " + str(chapter - 679))
+        chapter = chapter + 1
+    while 745 < chapter <= 797:
+        print("Jeremiah " + str(chapter - 745))
+        chapter = chapter + 1
+    while 797 < chapter <= 802:
+        print("Lamentations " + str(chapter - 797))
+        chapter = chapter + 1
+    while 802 < chapter <= 850:
+        print("Ezekiel " + str(chapter - 802))
+        chapter = chapter + 1
+    while 850 < chapter <= 862:
+        print("Daniel " + str(chapter - 850))
+        chapter = chapter + 1
+    while 862 < chapter <= 876:
+        print("Hosea " + str(chapter - 862))
+        chapter = chapter + 1
+    while 876 < chapter <= 879:
+        print("Joel " + str(chapter - 876))
+        chapter = chapter + 1
+    while 879 < chapter <= 888:
+        print("Amos " + str(chapter - 879))
+        chapter = chapter + 1
+    while 888 < chapter <= 889:
+        print("Obadiah " + str(chapter - 888))
+        chapter = chapter + 1
+    while 889 < chapter <= 893:
+        print("Jonah " + str(chapter - 889))
+        chapter = chapter + 1
+    while 893 < chapter <= 900:
+        print("Micah " + str(chapter - 893))
+        chapter = chapter + 1
+    while 900 < chapter <= 903:
+        print("Nahum " + str(chapter - 900))
+        chapter = chapter + 1
+    while 903 < chapter <= 906:
+        print("Habakkuk " + str(chapter - 903))
+        chapter = chapter + 1
+    while 906 < chapter <= 909:
+        print("Zephaniah " + str(chapter - 906))
+        chapter = chapter + 1
+    while 909 < chapter <= 911:
+        print("Haggai " + str(chapter - 909))
+        chapter = chapter + 1
+    while 911 < chapter <= 925:
+        print("Zechariah " + str(chapter - 911))
+        chapter = chapter + 1
+    while 925 < chapter <= 929:
+        print("Malachi " + str(chapter - 925))
+        chapter = chapter + 1
+    # Here is the Gospels
+    while 929 < chapter <= 957:
+        print("Matthew " + str(chapter - 929))
+        chapter = chapter + 1
+    while 957 < chapter <= 973:
+        print("Mark " + str(chapter - 957))
+        chapter = chapter + 1
+    while 973 < chapter <= 997:
+        print("Luke " + str(chapter - 973))
+        chapter = chapter + 1
+    while 997 < chapter <= 1018:
+        print("John " + str(chapter - 997))
+        chapter = chapter + 1
+    # Here is the 'History Book'
+    while 1018 < chapter <= 1046:
+        print("Acts " + str(chapter - 1018))
+        chapter = chapter + 1
+        # Here are the 'Pauline Epistles'
+    while 1046 < chapter <= 1062:
+        print("Romans " + str(chapter - 1046))
+        chapter = chapter + 1
+    while 1062 < chapter <= 1078:
+        print("1 Corinthians " + str(chapter - 1062))
+        chapter = chapter + 1
+    while 1078 < chapter <= 1091:
+        print("2 Corinthians " + str(chapter - 1078))
+        chapter = chapter + 1
+    while 1091 < chapter <= 1097:
+        print("Galatians " + str(chapter - 1091))
+        chapter = chapter + 1
+    while 1097 < chapter <= 1103:
+        print("Ephesians " + str(chapter - 1097))
+        chapter = chapter + 1
+    while 1103 < chapter <= 1107:
+        print("Philippians " + str(chapter - 1103))
+        chapter = chapter + 1
+    while 1107 < chapter <= 1111:
+        print("Colossians " + str(chapter - 1107))
+        chapter = chapter + 1
+    while 1111 < chapter <= 1116:
+        print("1 Thessalonians " + str(chapter - 1111))
+        chapter = chapter + 1
+    while 1116 < chapter <= 1119:
+        print("2 Thessalonians " + str(chapter - 1116))
+        chapter = chapter + 1
+    while 1119 < chapter <= 1125:
+        print("1 Timothy " + str(chapter - 1119))
+        chapter = chapter + 1
+    while 1125 < chapter <= 1129:
+        print("2 Timothy " + str(chapter - 1125))
+        chapter = chapter + 1
+    while 1129 < chapter <= 1132:
+        print("Titus " + str(chapter - 1129))
+        chapter = chapter + 1
+    while 1132 < chapter <= 1133:
+        print("Philemon " + str(chapter - 1132))
+        chapter = chapter + 1
+    # Here are the 'General Epistles'
+    while 1133 < chapter <= 1146:
+        print("Hebrews " + str(chapter - 1133))
+        chapter = chapter + 1
+    while 1146 < chapter <= 1151:
+        print("James " + str(chapter - 1146))
+        chapter = chapter + 1
+    while 1151 < chapter <= 1156:
+        print("1 Peter " + str(chapter - 1151))
+        chapter = chapter + 1
+    while 1156 < chapter <= 1159:
+        print("2 Peter " + str(chapter - 1156))
+        chapter = chapter + 1
+    while 1159 < chapter <= 1164:
+        print("1 John " + str(chapter - 1159))
+        chapter = chapter + 1
+    while 1164 < chapter <= 1165:
+        print("2 John " + str(chapter - 1164))
+        chapter = chapter + 1
+    while 1165 < chapter <= 1166:
+        print("3 John " + str(chapter - 1165))
+        chapter = chapter + 1
+    while 1166 < chapter <= 1167:
+        print("Jude " + str(chapter - 1166))
+        chapter = chapter + 1
+    # Here is the 'Apocalyptic Writing'
+    while 1167 < chapter <= 1189:
+        print("Revelation " + str(chapter - 1167))
+        chapter = chapter + 1
