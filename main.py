3c003c213f1574a320f133dd028ab6a1e9c978e0
Michael Maple <98138866+MikeMape@users.noreply.github.com>
2022-06-11 11:13:06 -0400

    Add files via upload
    Updated the Bible Chapters using for loops instead of while loops

---
+++ main.py
@@ -0,0 +1,127 @@
+for x in range(1, 51):
+    print("Genesis " + str(x))
+for x in range(1, 41):
+    print("Exodus " + str(x))
+for x in range(1, 28):
+    print("Leviticus " + str(x))
+for x in range(1, 37):
+    print("Numbers " + str(x))
+for x in range(1, 35):
+    print("Deuteronomy " + str(x))
+for x in range(1, 25):
+    print("Joshua " + str(x))
+for x in range(1, 22):
+    print("Judges " + str(x))
+for x in range(1, 5):
+    print("Ruth " + str(x))
+for x in range(1, 32):
+    print("1 Samuel " + str(x))
+for x in range(1, 25):
+    print("2 Samuel " + str(x))
+for x in range(1, 23):
+    print("1 Kings " + str(x))
+for x in range(1, 26):
+    print("2 Kings " + str(x))
+for x in range(1, 30):
+    print("1 Chronicles " + str(x))
+for x in range(1, 37):
+    print("2 Chronicles " + str(x))
+for x in range(1, 11):
+    print("Ezra " + str(x))
+for x in range(1, 14):
+    print("Nehemiah " + str(x))
+for x in range(1, 11):
+    print("Esther " + str(x))
+for x in range(1, 43):
+    print("Job " + str(x))
+for x in range(1, 151):
+    print("Psalms " + str(x))
+for x in range(1, 32):
+    print("Proverbs " + str(x))
+for x in range(1, 13):
+    print("Ecclesiastes " + str(x))
+for x in range(1, 9):
+    print("Song of Solomon " + str(x))
+for x in range(1, 67):
+    print("Isaiah " + str(x))
+for x in range(1, 53):
+    print("Jeremiah " + str(x))
+for x in range(1, 6):
+    print("Lamentations " + str(x))
+for x in range(1, 49):
+    print("Ezekiel " + str(x))
+for x in range(1, 13):
+    print("Daniel " + str(x))
+for x in range(1, 15):
+    print("Hosea " + str(x))
+for x in range(1, 4):
+    print("Joel " + str(x))
+for x in range(1, 10):
+    print("Amos " + str(x))
+print("Obadiah 1")
+for x in range(1, 5):
+    print("Jonah " + str(x))
+for x in range(1, 8):
+    print("Micah " + str(x))
+for x in range(1, 4):
+    print("Nahum " + str(x))
+for x in range(1, 4):
+    print("Habakkuk " + str(x))
+for x in range(1, 4):
+    print("Zephaniah " + str(x))
+for x in range(1, 3):
+    print("Haggai " + str(x))
+for x in range(1, 15):
+    print("Zechariah " + str(x))
+for x in range(1, 5):
+    print("Malachi " + str(x))
+for x in range(1, 29):
+    print("Matthew " + str(x))
+for x in range(1, 17):
+    print("Mark " + str(x))
+for x in range(1, 25):
+    print("Luke " + str(x))
+for x in range(1, 22):
+    print("John " + str(x))
+for x in range(1, 29):
+    print("Acts " + str(x))
+for x in range(1, 17):
+    print("Romans " + str(x))
+for x in range(1, 17):
+    print("1 Corinthians " + str(x))
+for x in range(1, 14):
+    print("2 Corinthians " + str(x))
+for x in range(1, 7):
+    print("Galatians " + str(x))
+for x in range(1, 7):
+    print("Ephesians " + str(x))
+for x in range(1, 5):
+    print("Philippians " + str(x))
+for x in range(1, 5):
+    print("Colossians " + str(x))
+for x in range(1, 6):
+    print("1 Thessalonians " + str(x))
+for x in range(1, 4):
+    print("2 Thessalonians " + str(x))
+for x in range(1, 7):
+    print("1 Timothy " + str(x))
+for x in range(1, 5):
+    print("2 Timothy " + str(x))
+for x in range(1, 4):
+    print("Titus " + str(x))
+print("Philemon 1")
+for x in range(1, 14):
+    print("Hebrews " + str(x))
+for x in range(1, 6):
+    print("James " + str(x))
+for x in range(1, 6):
+    print("1 Peter " + str(x))
+for x in range(1, 4):
+    print("2 Peter " + str(x))
+for x in range(1, 6):
+    print("1 John " + str(x))
+print("2 John 1")
+print("3 John 1")
+print("Jude 1")
+for x in range(1, 23):
+    print("Revelation " + str(x))
